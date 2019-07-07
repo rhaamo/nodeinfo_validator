1fec3341d40662a30c00b8747ce24701badf1375
Dashie <dashie@sigpipe.me>
2019-07-07 15:24:24 +0200

    Add deploy shit

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+set -e
+npm run build
+
+cd dist
+rm -rf .git
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:rhaamo/nodeinfo_validator.git master:gh-pages
+cd -
+
